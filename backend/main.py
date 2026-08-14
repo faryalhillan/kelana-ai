ec82e8f154b05551abb9ab48e2429da2351e3cb9
faryalhillan <faryalhillan@gmail.com>
2026-08-14 10:11:48 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-from services.trip_service import calculate_daily_budget, get_trip_category
+from services.trip_service import calculate_daily_budget, get_trip_category, get_transportation_recommendation, get_season
 
-def print_trip_summary(destination, country, days, budget, currency, travel_month, daily, category):
+def print_trip_summary(destinations, country, days, budget, currency, travel_month, daily, category):
     print("===================")
     print("KelanaAI")
     print("===================")
-    print(f"Destination  : {destination}")
+    print(f"Destination  : {destinations}")
     print(f"Country      : {country}")
     print(f"Duration     : {days} days")
     print(f"Budget       : {budget:.2f} {currency}")
@@ -15,15 +15,28 @@ def print_trip_summary(destination, country, days, budget, currency, travel_mont
     print("Recommended Places:")
     for place in recommended_places:
         print(f"- {place}")
+    print(f"Recommended Transportation: {get_transportation_recommendation(category)}")
+    print(f"Recommended Season: {get_season(travel_month)}")
 
 recommended_places = ["Tokyo Tower", "Shibuya", "Mount_Fuji"]
-destination = str(input("Enter your destination: "))
+
+destinations = []
+
+while True:
+    place = str(input("Enter a destination (or type 'done' to finish): "))
+
+    if place.lower() == 'done':
+        break
+    destinations.append(place)
+
+print("Full trip itenerary:", destinations)
 country = str(input("Enter the country of your destination: "))
 days = int(input("Enter the number of days for your trip: "))
 budget = float(input("Enter your budget: "))
 currency = str(input("Enter your preferred currency: "))
 travel_month = str(input("Enter the month you plan to travel: "))
 
+
 daily = calculate_daily_budget(budget, days)
 category = get_trip_category(budget)
-print_trip_summary(destination, country, days, budget, currency, travel_month, daily, category)
+print_trip_summary(destinations, country, days, budget, currency, travel_month, daily, category)
--- backend/services/trip_service.py
+++ backend/services/trip_service.py
@@ -8,3 +8,19 @@ def get_trip_category(budget):
         return "Standard"
     else:
         return "Luxury"
+
+def get_transportation_recommendation(category):
+    if category.lower() == "backpacker":
+        return "Bus"
+    elif category.lower() == "standard":
+        return "Train"
+    else:
+        return "Flight"
+
+def get_season(travel_month):
+    if travel_month.lower() == "December" or "12":
+        return("Peak Season")
+    elif travel_month.lower() == "June" or "6":
+        return("Holiday Season")
+    else:
+        return("Regular Season")
